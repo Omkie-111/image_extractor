c29f265d24c68df4513426da95239b92a8d60ffd
Omkie-111 <omkiepachauli@gmail.com>
2024-07-11 02:25:13 +0530

    Fixed few syntax errors

--- app/db/database.py
+++ app/db/database.py
@@ -1,5 +1,5 @@
-from sqlachemy import create_engine, MetaData
-from sqlachemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./test.db"
 

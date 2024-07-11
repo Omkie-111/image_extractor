a9c953f5c2bd04fb2cb7c633111617365fbcd7cd
Omkie-111 <omkiepachauli@gmail.com>
2024-07-12 03:25:51 +0530

    Updated Product table with serial number

--- app/db/models.py
+++ app/db/models.py
@@ -1,15 +1,11 @@
-from sqlalchemy import Column, Integer, String, Text
-from .database import Base
+from sqlalchemy import Column, Integer, String, Float
+from app.db.database import Base
 
 class Product(Base):
-    __tablename__ = "products"
-    
+    __tablename__ = 'products'
     id = Column(Integer, primary_key=True, index=True)
+    serial_number = Column(Integer, index=True)
     product_name = Column(String, index=True)
-    input_image_urls = Column(Text)
-    output_image_urls = Column(Text)
-    request_id = Column(String, unique=True, index=True)
-    status = Column(String, default="Pending")
-    
-    
-    
+    input_image_urls = Column(String, index=True)
+    status = Column(String, default="pending")
+    output_image_urls = Column(String, default="")

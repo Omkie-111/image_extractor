000d1fb3c7da7a3486feed2fd73b1f5372f3169b
Omkie-111 <omkiepachauli@gmail.com>
2024-07-12 00:38:58 +0530

    Added init function for metadata

--- app/db/database.py
+++ app/db/database.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./test.db"
 
@@ -7,6 +8,5 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-metadata = MetaData()
-
-
+def init_db():
+    Base.metadata.create_all(bind=engine)

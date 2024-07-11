72963f64f1772b2b21bca6634ccf087df9e03db7
Omkie-111 <omkiepachauli@gmail.com>
2024-07-12 03:25:19 +0530

    Updated database schema

--- app/db/schema.py
+++ app/db/schema.py
@@ -1,17 +1,23 @@
 from pydantic import BaseModel
 
 class ProductBase(BaseModel):
+    serial_number: int
     product_name: str
     input_image_urls: str
-    
+
 class ProductCreate(ProductBase):
     pass
 
 class Product(ProductBase):
     id: int
-    output_image_urls: str
-    request_id: str
     status: str
-    
+    output_image_urls: str
+
     class Config:
-        orm_mode = True
+        orm_mode: True
+
+class ProductList(BaseModel):
+    products: list[Product]
+
+    class Config:
+        orm_mode: True

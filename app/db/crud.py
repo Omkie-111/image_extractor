285d8eb4274ff7a41d5de01e0d95824c0e739de3
Omkie-111 <omkiepachauli@gmail.com>
2024-07-12 03:26:07 +0530

    Updated crud operations

--- app/db/crud.py
+++ app/db/crud.py
@@ -1,26 +1,26 @@
-from sqlalchemy.orm import Session 
-from . import models, schema
-import uuid
+from sqlalchemy.orm import Session
+from app.db.models import Product
+from app.db.schema import ProductCreate
 
-def create_product(db: Session, product: schema.ProductCreate):
-    db_product = models.Product(
-        product_name = product.product_name,
-        input_image_url = product.input_image_urls,
-        request_id = str(uuid.uuid4())
+def create_product(db: Session, product: ProductCreate):
+    db_product = Product(
+        serial_number=product.serial_number,
+        product_name=product.product_name,
+        input_image_urls=product.input_image_urls
     )
     db.add(db_product)
     db.commit()
     db.refresh(db_product)
     return db_product
 
-def get_product_by_request_id(db: Session, request_id: str):
-    return db.query(models.Product).filter(models.Product.request_id == request_id).first()
+def get_product_by_request_id(db: Session, request_id: int):
+    return db.query(Product).filter(Product.id == request_id).first()
 
-def update_product_status(db: Session, request_id: str, status: str, output_image_urls: str):
-    db_product = get_product_by_request_id(db, request_id)
-    db_product.status = status
-    db_product.output_image_urls = output_image_urls
-    db.commit()
-    db.refresh(db_product)
+def update_product_status(db: Session, request_id: int, status: str, output_image_urls: str = ""):
+    db_product = db.query(Product).filter(Product.id == request_id).first()
+    if db_product:
+        db_product.status = status
+        db_product.output_image_urls = output_image_urls
+        db.commit()
+        db.refresh(db_product)
     return db_product
-    
